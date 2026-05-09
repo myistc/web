d615bbe605eaf9b99c72cc8bb6c4624e6c53055f
Azmi Dev <integratedfolder@gmail.com>
2026-05-10 01:54:54 +0530

    Create faculty.ts

---
+++ src/types/faculty.ts
@@ -0,0 +1,9 @@
+export interface Faculty {
+  id: string;
+  name: string;
+  designation: string;
+  department: string;
+  qualification: string;
+  image_url: string | null;
+  created_at: string;
+}
